c539b1a340addc1afd5fa0fbb4cd3b6f077a0c1b
albertogiunta <alb.giunta@gmail.com>
2017-08-23 01:17:26 +0200

    fixed pro users number

--- app/src/main/java/com/jaus/albertogiunta/justintrain_oraritreni/utils/constants/CONST_FIREBASE.java
+++ app/src/main/java/com/jaus/albertogiunta/justintrain_oraritreni/utils/constants/CONST_FIREBASE.java
@@ -9,7 +9,7 @@ public class CONST_FIREBASE {
     public static final String FIREBASE_MAINTENANCE        = "maintenance_message";
     public static final String FIREBASE_LATEST_VERSION     = "latest_version";
     public static final String FIREBASE_UPDATE_MESSAGE     = "update_message";
-    public static final String FIREBASE_PRO_USERS_NUMBER   = "update_message";
+    public static final String FIREBASE_PRO_USERS_NUMBER   = "pro_users";
     public static final String FIREBASE_IS_DISCOUNT_SET    = "is_discount_set";
     public static final String FIREBASE_DISCOUNT_MESSAGE   = "discount_message";
 
